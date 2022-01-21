8434b873e8e4e5521b737d0917c9a85c9dc77f37
HoNam3202 <honam3202@gmail.com>
2022-01-21 19:04:39 +0700

    ...

--- backend/index.js
+++ backend/index.js
@@ -51,8 +51,8 @@ app.get('/*', (req, res) => {
 //     res.send(result);
 //   });
 // });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3001!');
 });
 
