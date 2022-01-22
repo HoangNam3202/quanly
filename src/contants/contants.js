6c9677845f8212783c27ee674c8f93194e978ba2
HoNam3202 <honam3202@gmail.com>
2022-01-22 19:49:01 +0700

    ...

--- src/contants/contants.js
+++ src/contants/contants.js
@@ -1,5 +1,5 @@
 // const API_URL = 'https://cafemanagerbmt.herokuapp.com';
 // const API_URL = 'http://192.168.1.11:3001';
-const API_URL = '' || 'http://192.168.1.11:3001';
+const API_URL = 'http://192.168.1.11:3001' || '';
 //192.168.1.11
 export default API_URL;
